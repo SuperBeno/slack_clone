b71625bd03fe40d6e13b6ce4b26d3629d043f110
SuperBeno <malingre.benjamin@outlook.fr>
2020-08-30 14:45:17 +0200

    Starting Chat component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey</h1>
-
-      {/**Header */}
-      <Header></Header>
-      <div className="app__body">
-        <Sidebar></Sidebar>
-      </div>
-      {/**React-ROuter */}
+      <Router>
+        <Header></Header>
+        <div className="app__body">
+          <Sidebar></Sidebar>
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat></Chat>
+            </Route>
+            <Route path="/">
+              <h1>Welcom do the show</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
